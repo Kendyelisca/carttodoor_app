83ab407128263f5181d29c9e6b5f5f96e1170559
Kendy <kendyelisca5@gmail.com>
2023-09-19 23:36:38 -0500

    footer details added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,68 @@
 import "./footer.css";
-
+import { BsFacebook } from "react-icons/bs";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
 const Footer = () => {
   return (
-    <footer>
-      <div className="footer-content">
-        <h3>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
-          quibusdam inventore ex aliquam reiciendis libero, sed ipsum ipsa
-          adipisci blanditiis molestiae cupiditate, officiis maxime dignissimos,
-          praesentium ipsam hic! Ab, architecto.
-        </h3>
-        <p>&copy; 2023 Easy Shopping. All rights reserved.</p>
+    <div className="footer-content">
+      <div className="list-container">
+        <div className="list">
+          <b>Here to Help</b>
+          <p>
+            <a href="">Help and FAQs</a>
+          </p>
+          <p>
+            <a href="">Contact us</a>
+          </p>
+        </div>
+        <div className="list">
+          <b>About CarttoDoorMarket</b>
+          <p>
+            <a href="">Store vacancies</a>
+          </p>
+          <p>
+            <a href="">Careers</a>
+          </p>{" "}
+          <p>
+            <a href="">Sustainability</a>
+          </p>
+        </div>
+        <div className="list">
+          <b>Our website</b>
+          <p>
+            <a href="">Terms & Conditions</a>
+          </p>
+          <p>
+            <a href="">Privacy & Cookie Policy</a>
+          </p>
+          <p>
+            <a href="">Privacy Center</a>
+          </p>
+          <p>
+            <a href="">Accessibility</a>
+          </p>
+        </div>
       </div>
-    </footer>
+      <div className="socials">
+        <p>Follow us</p>
+        <a href="">
+          <BsFacebook />
+        </a>
+        <a href="">
+          <AiFillTwitterCircle />
+        </a>
+        <a href="">
+          <AiFillInstagram />
+        </a>
+        <a href="">
+          <AiFillYoutube />
+        </a>
+      </div>
+      <p className="rights">
+        &copy; cartToDoorMarket.com 2023 All rights reserved
+      </p>
+    </div>
   );
 };
 
