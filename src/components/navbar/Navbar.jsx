0db946caa1217dabd7acab4b2dbf994d8f99e082
Kendy <kendyelisca5@gmail.com>
2023-10-19 10:45:56 -0500

    optimized responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,19 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 import { PiShoppingCart } from "react-icons/pi";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { CgProfile } from "react-icons/cg";
-
+import { FaTimes, FaBars } from "react-icons/fa";
 import { UserContext } from "../../contexts/user-context";
 const Navbar = () => {
   const { user } = useContext(UserContext) ?? {}; // Provide an empty object as a default
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-  console.log(user);
-  // Calculate the total quantity of items in the cart
+  // Function to toggle the menu
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div className="navbar">
@@ -42,11 +45,27 @@ const Navbar = () => {
           <CgProfile />
           {user && user.userName && <div className="user">{user.userName}</div>}
         </Link>
-        <div className="menu">
-          {" "}
-          <RxHamburgerMenu />
+        <div className="menu" onClick={toggleMenu}>
+          {isMenuOpen ? <FaTimes /> : <FaBars />}
         </div>
       </nav>
+      {isMenuOpen && (
+        <div className="menu-options">
+          {user && user.userName && <li className="user">{user.userName}</li>}
+          <li>
+            <Link to="/aboutus">About Us</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/account">Account</Link>
+          </li>
+        </div>
+      )}
     </div>
   );
 };
