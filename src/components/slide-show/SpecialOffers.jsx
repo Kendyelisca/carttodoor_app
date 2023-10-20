2348d0f9e0486fe39dd3df40bdb522aba6bc5585
Kendy <kendyelisca5@gmail.com>
2023-10-20 11:31:19 -0500

    added offers slide show

--- src/components/slide-show/SpecialOffers.jsx
+++ src/components/slide-show/SpecialOffers.jsx
@@ -1,6 +1,6 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "./SpecialOffers.css"; // Import the CSS file
+import "./specialOffers.css"; // Import the CSS file
 const SpecialOffers = () => {
   const specialOffers = [
     {
